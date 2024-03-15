38c6c3cf28a2f18f80ec76a046063a7df5b2fee7
KOSASIH <kosasihg88@gmail.com>
2024-03-15 23:36:50 +0700

    Update my_super_ai.py

--- my_super_ai.py
+++ my_super_ai.py
@@ -1,8 +1,8 @@
 import superai as ai
 
-client = ai.Client("2nodwtr18uvrqcskehvbqnu9geb85wgb4n8wuwc5uch9r532vkqqs577ak0stozn") 
+client = ai.Client("your_api_key") 
 
 client.create_jobs(
-    app_id="63f4e18afd5a1b78762767ea",
+    app_id="your_project_id",
     inputs=[{"image_url":"https://cdn.super.ai/cool-bulldog.jpg"},{"image_url":"https://cdn.super.ai/hot-dog-01.jpeg"}]
 )
